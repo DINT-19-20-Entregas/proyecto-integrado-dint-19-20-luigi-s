f312a4f898b11efc4873f5dc211d587e7390eef6
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2020-01-24 12:55:42 +0100

    Creacion vistas Elementos y Categoria

---
+++ Proyecto-Restaurante/Proyecto-Restaurante/User Controls/UserControlElementos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_Restaurante.User_Controls
+{
+    /// <summary>
+    /// Lógica de interacción para UserControlElementos.xaml
+    /// </summary>
+    public partial class UserControlElementos : UserControl
+    {
+        public UserControlElementos()
+        {
+            InitializeComponent();
+        }
+    }
+}
