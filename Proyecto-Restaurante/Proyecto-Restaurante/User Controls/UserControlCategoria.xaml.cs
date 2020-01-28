ccf2400b136af6a6f570ba5b5cc32e21624c27d6
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2020-01-28 12:40:13 +0100

    Añado la clase servicioBD para conectar con la BD

--- Proyecto-Restaurante/Proyecto-Restaurante/User Controls/UserControlCategoria.xaml.cs
+++ Proyecto-Restaurante/Proyecto-Restaurante/User Controls/UserControlCategoria.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Restaurante.Vista_Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Proyecto_Restaurante.User_Controls
         public UserControlCategoria()
         {
             InitializeComponent();
+            DataContext = new CategoriaVM();
         }
     }
 }
