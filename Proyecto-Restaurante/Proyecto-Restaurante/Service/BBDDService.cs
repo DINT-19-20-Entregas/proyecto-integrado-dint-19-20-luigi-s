8125ddafa4d9a4d1872412cb1d10a6c74fb2c255
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2020-01-29 13:31:26 +0100

    Hago la clase BBDService

--- Proyecto-Restaurante/Proyecto-Restaurante/Service/BBDDService.cs
+++ Proyecto-Restaurante/Proyecto-Restaurante/Service/BBDDService.cs
@@ -11,17 +11,16 @@ namespace Proyecto_Restaurante.Servicios
 {
     public static class BBDDService
     {
-        private static readonly Project_SQLEntities _contexto;
+        private static readonly Project_SQLEntities _contexto = new Project_SQLEntities();
 
         static BBDDService()
         {
-            _contexto = new Project_SQLEntities();
             _contexto.Categorias.Load();
             _contexto.elementosCarta.Load();
             _contexto.Pedidos.Load();
         }
 
-        private static ObservableCollection<Categorias> GetCategorias()
+        public static ObservableCollection<Categorias> GetCategorias()
         {
             return _contexto.Categorias.Local;
         }
@@ -31,11 +30,60 @@ namespace Proyecto_Restaurante.Servicios
             return _contexto.elementosCarta.Local;
         }
 
-        private static ObservableCollection<Pedidos> getPedidos()
+        public static ObservableCollection<Pedidos> GetPedidos()
         {
             return _contexto.Pedidos.Local;
         }
 
+        public static ObservableCollection<elementosCarta> GetElementosCarta()
+        {
+            return _contexto.elementosCarta.Local;
+        }
 
+        public static ObservableCollection<Categorias> GetElementosCategorias()
+        {
+            return _contexto.Categorias.Local;
+        }
+
+        public static int AddPedido(Pedidos p)
+        {
+            _contexto.Pedidos.Add(p);
+            return _contexto.SaveChanges();
+        }
+
+        public static int AddElementoCarta(elementosCarta e)
+        {
+            _contexto.elementosCarta.Add(e);
+            return _contexto.SaveChanges();
+        }
+
+        public static int AddCategoria(Categorias c)
+        {
+            _contexto.Categorias.Add(c);
+            return _contexto.SaveChanges();
+        }
+
+        public static int DeletePedido(Pedidos p)
+        {
+            _contexto.Pedidos.Remove(p);
+            return _contexto.SaveChanges();
+        }
+
+        public static int DeleteElementosCarta(elementosCarta e)
+        {
+            _contexto.elementosCarta.Remove(e);
+            return _contexto.SaveChanges();
+        }
+
+        public static int DeleteCategoria(Categorias c)
+        {
+            _contexto.Categorias.Remove(c);
+            return _contexto.SaveChanges();
+        }
+
+        public static int UpdateDB()
+        {
+            return _contexto.SaveChanges();
+        }
     }
 }
