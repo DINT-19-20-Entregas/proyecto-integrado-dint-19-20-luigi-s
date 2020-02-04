b8a2aecad9e798f8426f85ff45f809f13f5abf24
Sergio Ramos Santonja <sergioramos55@outlook.es>
2020-02-04 12:52:14 +0100

    Termino de enlazar binding de pedidosy categorias, se ve correctamente.

--- Proyecto-Restaurante/Proyecto-Restaurante/UsersControl/UserControlCategoria.xaml.cs
+++ Proyecto-Restaurante/Proyecto-Restaurante/UsersControl/UserControlCategoria.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Restaurante.Vista_Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Proyecto_Restaurante.UsersControls
         public UserControlCategoria()
         {
             InitializeComponent();
+            this.DataContext = new CategoriaVM();
         }
     }
 }
