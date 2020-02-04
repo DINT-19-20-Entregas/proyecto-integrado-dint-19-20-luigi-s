1c719f6bb53487e820fe2b5e4c3c2d5bb700b4a0
Ismael Collado <ismael.collado@iesdoctorbalmis.com>
2020-02-04 21:37:10 +0100

    Bindings y commandos Categoria

--- Proyecto-Restaurante/Proyecto-Restaurante/Vista Modelo/CategoriaVM.cs
+++ Proyecto-Restaurante/Proyecto-Restaurante/Vista Modelo/CategoriaVM.cs
@@ -22,5 +22,34 @@ namespace Proyecto_Restaurante.Vista_Modelo
             Categorias = BBDDService.GetCategorias();
         }
 
+        public void Añadir(Categorias c)
+        {
+            BBDDService.AddCategoria(c);
+            MessageBox.Show("Categoria añadida", "Confirmation", MessageBoxButton.OK , MessageBoxImage.Information);
+        }
+
+        public void Eliminar(Categorias c)
+        {
+            BBDDService.DeleteCategoria(c);
+            MessageBox.Show("Categoria eliminada", "Confirmation", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public bool Existe(Categorias c)
+        {
+            bool existe = false;
+            foreach (Categorias cLista in ListaCategorias)
+            {
+                if (cLista.nombreCategoria.Equals(c.nombreCategoria))
+                    existe = true;
+            }
+
+            return existe;
+        }
+
+        public void Guardar()
+        {
+            BBDDService.UpdateDB();
+        }
+
     }
 }
