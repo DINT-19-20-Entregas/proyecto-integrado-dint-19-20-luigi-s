fbb7cc5da4fc0e9ab55bfe32175ea3d14c5f4c86
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2020-02-16 14:48:37 +0100

    Hago la consulta del iva al apirest

--- Proyecto-Clientes/Proyecto-Clientes/VistaModelo/PrecioVM.cs
+++ Proyecto-Clientes/Proyecto-Clientes/VistaModelo/PrecioVM.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
 
 namespace Proyecto_Clientes.VistaModelo
 {
-    class PrecioVM
+    public class PrecioVM
     {
         public PrecioVM()
         {
+            
+        }
 
+        public static int GetIva()
+        {
+            string iva = "";
+            string URL = "https://apidint1920.azurewebsites.net/api/iva";
+            var json = new WebClient().DownloadString(URL);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            iva = m.iva; // Aqui asigo a iva el valor que encuentre en el JSON con el nombre "iva", en este caso 10
+            return Int32.Parse(iva);
         }
     }
 }
